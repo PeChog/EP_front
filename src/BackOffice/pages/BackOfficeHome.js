2673ce397cd934e4c3efa1f48967b8df65a292b7
Sofiane Rehila <sofiane.rehila@gmail.com>
2022-08-26 20:14:24 +0200

    pdf

--- src/BackOffice/pages/BackOfficeHome.js
+++ src/BackOffice/pages/BackOfficeHome.js
@@ -1,40 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 const BackofficeHome = ({ handleToken }) => {
   const navigate = useNavigate();
-  const [pdf, setPdf] = useState();
 
-  // const handlePdf = async () => {
-  //   try {
-  //     const formData = new FormData();
-
-  //     formData.append("pdfFile", pdf);
-
-  //     const response = await axios.post(
-  //       "http://localhost:3000/upload/pdf",
-  //       formData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "multipart/form-data",
-  //         },
-  //       }
-  //     );
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
   return (
     <div>
-      <button
-        onClick={() => {
-          handleToken(null);
-          navigate("/auth");
-        }}>
-        Deconnexion
-      </button>
       <Link to={"/"}>Retour sur le site</Link>
 
       <h1 style={{ textAlign: "center" }}>BackOffice</h1>
@@ -46,19 +19,7 @@ const BackofficeHome = ({ handleToken }) => {
 
         <Link to={"/CrudImages"}>Creer, Modifier, Supprimer des images</Link>
 
-        {/* <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handlePdf();
-          }}>
-          <input
-            type="file"
-            onChange={(e) => {
-              setPdf(e.target.files[0]);
-            }}
-          />
-          <input type="submit" />
-        </form> */}
+        <Link to={"/addPdf"}>Ajouter une brochure</Link>
       </div>
     </div>
   );
